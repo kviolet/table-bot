1664dbea0281515c8e3c0b59bad93ab3cb5f1c26
KleptomaniacViolet <KleptomaniacViolet@(none)>
2013-09-30 17:45:00 +0100

    refactor variables and constants

--- data-tables-update.py
+++ data-tables-update.py
@@ -10,14 +10,16 @@ site = pywikibot.Site("en", "wikipedia")
 conn = sqlite3.connect("pages.db")
 
 PREFIX = "KleptomaniacViolet/Language families data/"
+PREFIX_NS = 2
 DATA_PREFIX = "Sandbox/KleptomaniacViolet/Language families/Data/"
+DATA_PREFIX_NS = 828
 DRY_RUN = True
 
 #Convention for dry running: slashes in the page title are replaced with %, % is replaced by %%.
 def dry_run_file(title):
     return title.replace("%", "%%").replace("/", "%")
 
-cookie_page = pywikibot.Page(site, DATA_PREFIX + "Cookie", ns = 828)
+cookie_page = pywikibot.Page(site, DATA_PREFIX + "Cookie", ns = DATA_PREFIX_NS)
 
 def remove_prefix(s):
     return s[len(PREFIX):]
@@ -25,7 +27,7 @@ def remove_prefix(s):
 all_stripped_titles = set()
 hierarchies = []
 for page in site.allpages(prefix = PREFIX,
-                          namespace = 2,
+                          namespace = PREFIX_NS,
                           content = True):
     contents = page.get()
     stripped_title = remove_prefix(page.title(withNamespace = False))
@@ -70,7 +72,7 @@ for stripped_title, revid, contents, new_cookie, hs in hierarchies:
     for n, lua in enumerate(map(gen_lua_wrapping, chunk(gen_lua_iter(hs)))):
         title = "%s%s/%s/%d" % (DATA_PREFIX, stripped_title, new_cookie, n)
         if not DRY_RUN:
-            page = pywikibot.Page(site, title, ns = 828)
+            page = pywikibot.Page(site, title, ns = DATA_PREFIX_NS)
             page.text = lua
             page.save(comment = "Updating language family tables")
         else:
@@ -88,9 +90,10 @@ for stripped_title in all_stripped_titles:
         pagetitle = "Module:%s%s/%s/%d" % (DATA_PREFIX, stripped_title, new_cookie, n)
         cookies.append(lua_string(pagetitle))
 
+text = 'return { %s }' % ", ".join(cookies)
 if not DRY_RUN:
-    cookie_page.text = 'return { %s }' % ", ".join(cookies)
+    cookie_page.text = text
     cookie_page.save(comment = "Switching to the updated tables")
 else:
     with open("generated/%s" % dry_run_file(cookie_page.title(withNamespace = False)), "w") as f:
-        f.write(('return { %s }' % ", ".join(cookies)).encode("utf-8"))
+        f.write(text.encode("utf-8"))
