9b2de18273869c4b08d51a5766c9836a76ea44fc
KleptomaniacViolet <KleptomaniacViolet@(none)>
2013-09-30 19:07:40 +0100

    Store alpha/beta status on-wiki

--- data-tables-update.py
+++ data-tables-update.py
@@ -1,20 +1,21 @@
 import pywikibot
 import sqlite3
+import unicodecsv as csv
+from cStringIO import StringIO
 from lua_table_generate import parse, gen_lua_iter, gen_lua_wrapping, lua_string
-
-#XXX:
-#This is not safe if more than one instance (with separate databases) is in operation, because the alpha/beta status is tracked there. Solution: store the alpha/beta cookie for each subpage on-wiki somewhere?
 
 site = pywikibot.Site("en", "wikipedia")
 
 #Schema:
-#  CREATE TABLE last_good (title STRING PRIMARY KEY, revid INTEGER NOT NULL, contents STRING NOT NULL, cookie STRING NOT NULL, lua_page_count INTEGER NOT NULL);
+#  CREATE TABLE last_good (title STRING PRIMARY KEY, revid INTEGER NOT NULL, contents STRING NOT NULL, lua_page_count INTEGER NOT NULL);
 #  CREATE TABLE last_attempted (title STRING PRIMARY KEY, revid INTEGER NOT NULL);
 conn = sqlite3.connect("pages.db")
 
 PREFIX = "KleptomaniacViolet/Language families data/"
 PREFIX_NS = 2
-DATA_PREFIX = "Sandbox/KleptomaniacViolet/Language families/Data/"
+COOKIE_PAGE = "KleptomaniacViolet/Language families cookies"
+COOKIE_NS = 2
+DATA_PREFIX = "Sandbox/KleptomaniacViolet/Language families/Data"
 DATA_PREFIX_NS = 828
 DRY_RUN = True
 
@@ -22,31 +23,48 @@ DRY_RUN = True
 def dry_run_file(title):
     return title.replace("%", "%%").replace("/", "%")
 
-cookie_page = pywikibot.Page(site, DATA_PREFIX + "Cookie", ns = DATA_PREFIX_NS)
+table_list_page = pywikibot.Page(site, DATA_PREFIX, ns = DATA_PREFIX_NS)
+
+old_cookies = {}
+cookie_page = pywikibot.Page(site, COOKIE_PAGE, ns = COOKIE_NS)
+try:
+    cookie_data = cookie_page.get()
+except pywikibot.NoPage:
+    pass
+else:
+    if cookie_data == "LOCKED":
+        raise ValueError("Some other instance is mid-update.")
+    if not DRY_RUN:
+        cookie_page.text = "LOCKED"
+        cookie_page.save(comment = "Claiming the lock.")
+    try:
+        old_cookies = dict(csv.reader(cookie_data, encoding="utf-8"))
+    except:
+        print "Uh-oh, can't parse the cookie page. Did someone manually edit it? Consider reverting."
+        raise
 
 def remove_prefix(s):
     return s[len(PREFIX):]
 
-all_stripped_titles = set()
+all_stripped_titles = {}
 hierarchies = []
 for page in site.allpages(prefix = PREFIX,
                           namespace = PREFIX_NS,
                           content = True):
     contents = page.get()
     stripped_title = remove_prefix(page.title(withNamespace = False))
-    all_stripped_titles.add(stripped_title)
     revid = page.latestRevision()
-    res = conn.execute("SELECT revid, cookie, lua_page_count FROM last_good WHERE title = ?", (stripped_title,)).fetchone()
-    if res is None:
+    res = conn.execute("SELECT revid FROM last_good WHERE title = ?", (stripped_title,)).fetchone()
+    if stripped_title not in old_cookies:
         new_cookie = "alpha"
     else:
-        cookie = res[1]
-        if cookie == "alpha":
+        if old_cookies[stripped_title] == "alpha":
             new_cookie = "beta"
         else:
             new_cookie = "alpha"
-        if int(res[0]) == revid:
-            continue
+    all_stripped_titles[stripped_title] = new_cookie
+    if res is not None and int(res[0]) == revid:
+        continue
     conn.execute("INSERT OR REPLACE INTO last_attempted (title, revid) VALUES (?, ?)", (stripped_title, revid))
     conn.commit()
     try:
@@ -73,7 +91,7 @@ def chunk(i):
 
 for stripped_title, revid, contents, new_cookie, hs in hierarchies:
     for n, lua in enumerate(map(gen_lua_wrapping, chunk(gen_lua_iter(hs)))):
-        title = "%s%s/%s/%d" % (DATA_PREFIX, stripped_title, new_cookie, n)
+        title = "%s/%s/%s/%d" % (DATA_PREFIX, stripped_title, new_cookie, n)
         if not DRY_RUN:
             page = pywikibot.Page(site, title, ns = DATA_PREFIX_NS)
             page.text = lua
@@ -81,22 +99,35 @@ for stripped_title, revid, contents, new_cookie, hs in hierarchies:
         else:
             with open("generated/%s" % dry_run_file(title), "w") as f:
                 f.write(lua.encode("utf-8"))
-    conn.execute("INSERT OR IGNORE INTO last_good (title, revid, contents, cookie, lua_page_count) VALUES (?, ?, ?, ?, ?)", (stripped_title, revid, contents, new_cookie, n))
+    conn.execute("INSERT OR IGNORE INTO last_good (title, revid, contents, lua_page_count) VALUES (?, ?, ?, ?)", (stripped_title, revid, contents, n))
     conn.commit()
 
-cookies = []
+pages = []
 for stripped_title in all_stripped_titles:
     res = conn.execute("SELECT lua_page_count FROM last_good WHERE title = ?", (stripped_title,)).fetchone()
     if res is None:
         continue
     for n in range(0, int(res[0]) + 1):
-        pagetitle = "Module:%s%s/%s/%d" % (DATA_PREFIX, stripped_title, new_cookie, n)
-        cookies.append(lua_string(pagetitle))
+        pagetitle = "Module:%s/%s/%s/%d" % (DATA_PREFIX, stripped_title, new_cookie, n)
+        pages.append(lua_string(pagetitle))
 
-text = 'return { %s }' % ", ".join(cookies)
+text = 'return { %s }' % ", ".join(pages)
 if not DRY_RUN:
+    table_list_page.text = text
+    table_list_page.save(comment = "Switching to the updated tables")
+else:
+    with open("generated/%s" % dry_run_file(table_list_page.title(withNamespace = False)), "w") as f:
+        f.write(text.encode("utf-8"))
+
+f = StringIO()
+w = csv.writer(f, encoding = "utf-8")
+for row in all_stripped_titles.items():
+    w.writerow(row)
+text = f.getvalue().decode("utf-8")
+
+if not DRY_RUN:    
     cookie_page.text = text
-    cookie_page.save(comment = "Switching to the updated tables")
+    cookie_page.save(comment = "Updating and unlocking the cookie list")
 else:
     with open("generated/%s" % dry_run_file(cookie_page.title(withNamespace = False)), "w") as f:
         f.write(text.encode("utf-8"))
